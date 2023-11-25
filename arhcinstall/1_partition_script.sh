238efba16b7ac59fad62fad283ebfa2655980210
Untitled14 <31729713+Untitled14@users.noreply.github.com>
2023-11-25 21:11:16 +0200

    Update 1_partition_script.sh

--- arhcinstall/1_partition_script.sh
+++ arhcinstall/1_partition_script.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 sgdisk --zap-all /dev/sda
+sgdisk --zap-all /dev/sdb
 
 ## Partition the disks
 # Replace /dev/sda (lsblk)
@@ -10,20 +11,21 @@ parted /dev/sda mklabel gpt mkpart boot fat32 1MiB 1024MiB
 parted /dev/sda set 1 esp on
 
 # Swap partition (36 GiB)
-parted /dev/sda mkpart swap linux-swap 1024MiB 2GiB 
+parted /dev/sda mkpart swap linux-swap 1024MiB 37GiB 
 
 # Root partition (128 GiB)
-parted /dev/sda mkpart root ext4 2GiB 20GiB
+parted /dev/sda mkpart root ext4 37GiB 128GiB
 
 # Home partition (Remaining space)
-parted /dev/sda mkpart home ext4 20GiB 100% 
-
+parted /dev/sda mkpart extra ext4 128GiB 100%
+parted /dev/sdb mkpart home ext4 2MiB 100%
 
 # Format the partitions
 mkfs.fat -F 32 /dev/sda1
 mkswap /dev/sda2
 mkfs.ext4 /dev/sda3
 mkfs.ext4 /dev/sda4
+mkfs.ext4 /dev/sdb1
 
 swapon /dev/sda2
 
